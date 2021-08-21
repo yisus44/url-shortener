3ef599ac8b7390aae80c13e2a4ee20cc3a3fa0c5
Jesus Flores <jesusadrian1953.1@gmail.com>
2021-08-21 14:35:19 -0500

    compiled

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config({ path: __dirname + '/.env' });
 var app_1 = require("./app");
+var throng_1 = __importDefault(require("throng"));
 var PORT = Number(process.env.PORT) || 8000;
-app_1.app.listen(PORT, function () {
-    console.log('Server up and running on port', PORT);
-});
+var WORKERS = Number(process.env.WEB_CONCURRENCY) || 1;
+throng_1.default(function () { return start(); });
+function start() {
+    app_1.app.listen(PORT, function () {
+        console.log('Server up and running on port', PORT);
+    });
+}
