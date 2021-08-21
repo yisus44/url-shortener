f2a590523b1d1ddb4239fdf9806c27acd8e384ad
Jesus Flores <jesusadrian1953.1@gmail.com>
2021-08-21 17:51:07 -0500

    improved clustering and added node types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-
+import { cpus } from 'os';
+import process from 'process';
+import cluster from 'cluster';
 import { app } from './app';
-import throng from 'throng';
-
+const numCPUs = cpus().length;
 const PORT: number = Number(process.env.PORT) || 8000;
-const WORKERS = Number(process.env.WEB_CONCURRENCY) || 1;
+//TODO: Change it to .isPrimary when 16 or more become stable
+if (cluster.isMaster) {
+  console.log(`Primary ${process.pid} is running`);
 
-throng(() => start());
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-function start() {
+  cluster.on('exit', (worker, code, signal) => {
+    //Loggin stuff
+    console.log(`Worker ${worker} endend, reasons:`);
+    if (signal) {
+      console.log(`worker was killed by signal: ${signal}`);
+    } else if (code !== 0) {
+      console.log(`worker exited with error code: ${code}`);
+    } else {
+      console.log('worker success!');
+    }
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
   app.listen(PORT, function () {
     console.log('Server up and running on port', PORT);
   });
