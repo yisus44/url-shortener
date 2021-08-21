325dbd8b5045e8cbcadd4b03927102233002f8b1
Jesus Flores <jesusadrian1953.1@gmail.com>
2021-08-21 14:32:45 -0500

    adding clustering

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 require('dotenv').config({ path: __dirname + '/.env' });
 
 import { app } from './app';
-const PORT: number = Number(process.env.PORT) || 8000;
+import throng from 'throng';
 
-app.listen(PORT, function () {
-  console.log('Server up and running on port', PORT);
-});
+const PORT: number = Number(process.env.PORT) || 8000;
+const WORKERS = Number(process.env.WEB_CONCURRENCY) || 1;
+
+throng(() => start());
+
+function start() {
+  app.listen(PORT, function () {
+    console.log('Server up and running on port', PORT);
+  });
+}
